7a92c105764aa5803129a2a50109e5dee1c6dae6
AkhileshHjp <tech.kr.akhi@gmail.com>
2024-10-15 13:29:21 +0530

    PORT

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
-const io = require('socket.io')(PORT, {
+const io = require('socket.io')(3000, {
   cors: {
     origin: '*',
   },
