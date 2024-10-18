0f0892f05aeaf002e8c8dc8a3a38a7f670c4c757
AkhileshHjp <tech.kr.akhi@gmail.com>
2024-10-18 10:22:59 +0530

    chnagess

--- index.js
+++ index.js
@@ -73,6 +73,6 @@ io.on('connection', (socket) => {
 });
 
 // Start the server on port 3000
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
